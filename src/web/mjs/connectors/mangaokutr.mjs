6bf0435d2fd6a56244a60a86ba21d65044176cbe
Furkangcmn <97408699+Furkangcmn@users.noreply.github.com>
2022-11-13 13:32:48 +0300

    new site

---
+++ src/web/mjs/connectors/mangaokutr.mjs
@@ -0,0 +1,14 @@
+import Connector from '../engine/Connector.mjs';
+
+export default class MangaOkuTr extends Connector {
+        
+            /**
+             *
+             */
+            constructor() {
+                super();
+                super.id = 'mangaokutr';
+                super.label = 'Manga Oku TR';
+                this.tags = [ 'manga', 'webtoon', 'turkish' ];
+                this.url = 'https://mangaokutr.com';
+            }
